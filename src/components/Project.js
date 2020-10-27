7bb6161e5cbd4de92f5265caad460815c6e6e23d
SAbbas2018 <raza.35.abbas@gmail.com>
2020-10-27 12:54:19 -0400

    added media queries for up to 4k display

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,18 @@
 import React from "react";
 function Project(props) {
   const { project } = props;
+  const { tech } = project;
   return (
     <div className="card">
       <div className={`card-img-top ${project.name}`}></div>
       <div className="card-body">
         <h5 className="card-title">{project.name}</h5>
         <p className="card-text">{project.desc}</p>
+        <div className="project-tech">
+          {tech.map((technology) => {
+            return <div className="technology">{technology}</div>;
+          })}
+        </div>
         <div className="project-links">
           <a
             href={project.repo}
