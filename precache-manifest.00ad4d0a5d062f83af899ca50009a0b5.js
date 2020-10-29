d66e9339b3e250cc5785bbf9fff9f3ae0b43b0ea
Syed Raza Abbas <raza.35.abbas@gmail.com>
2020-10-28 23:06:02 -0400

    Updates

---
+++ precache-manifest.00ad4d0a5d062f83af899ca50009a0b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350b5676ac23a08e7caf7f817d1268e3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8da8fbc8dd11c62d0cbe",
+    "url": "/portfolio/static/css/main.4e672cd0.chunk.css"
+  },
+  {
+    "revision": "04281863c20fd19f61b4",
+    "url": "/portfolio/static/js/2.cd4b8103.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.cd4b8103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da8fbc8dd11c62d0cbe",
+    "url": "/portfolio/static/js/main.ac1cce9e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "515ea5998aae5c8c8061fb33c57b92e0",
+    "url": "/portfolio/static/media/mainHomeBg.515ea599.jpg"
+  }
+]);
