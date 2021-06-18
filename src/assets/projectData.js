f498fe439cb9ac6fd60c07643b662f7f9d98e4fe
Syed Raza Abbas <raza.35.abbas@gmail.com>
2021-06-18 13:11:39 -0400

    v2 in progress

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -1,48 +1,60 @@
 import React from "react";
 const projectData = [
-  {
-    id: 1,
-    name: "BookCase",
-    live: "https://book-case-sa.herokuapp.com/",
-    image: "/assets/bookcase.JPG",
-    repo: "https://github.com/SAbbas2018/BookCase",
-    desc:
-      "This MERN stack application uses the Google Books API for getting information on books by their ISBN-13s, and makes recommendations to users.",
-    tech: [
-      <i className="devicon-mongodb-plain-wordmark tech-icon"></i>,
-      <i className="devicon-express-original-wordmark tech-icon"></i>,
-      <i className="devicon-react-original-wordmark tech-icon"></i>,
-      <i className="devicon-nodejs-plain-wordmark tech-icon"></i>,
-    ],
-  },
-  {
-    id: 2,
-    name: "TicTacToe",
-    live: "https://tictactoe-sa.herokuapp.com/",
-    image: "/assets/tictactoe.JPG",
-    repo: "https://github.com/SAbbas2018/tictactoe",
-    desc:
-      "An OOP Tic Tac Toe game users can play against another user on the same window, a computer with 3 levels of difficulty, or agains a friend in real time through web sockets.",
-    tech: [
-      <i className="devicon-express-original-wordmark tech-icon"></i>,
-      <i className="devicon-react-original-wordmark tech-icon"></i>,
-      <i className="devicon-nodejs-plain-wordmark tech-icon"></i>,
-    ],
-  },
-  {
-    id: 3,
-    name: "NewScript",
-    live: "https://newscript-sa.herokuapp.com/",
-    image: "/assets/newscript.JPG",
-    repo: "https://github.com/SAbbas2018/newscript-react",
-    desc:
-      "This React and Node.js application obtains weather information for a given city, and news headlines for common categories.",
-    tech: [
-      <i className="devicon-express-original-wordmark tech-icon"></i>,
-      <i className="devicon-react-original-wordmark tech-icon"></i>,
-      <i className="devicon-nodejs-plain-wordmark tech-icon"></i>,
-    ],
-  },
+	{
+		id: 1,
+		name: "BookCase",
+		live: "https://book-case-sa.herokuapp.com/",
+		image: "/assets/bookcase.JPG",
+		repo: "https://github.com/SAbbas2018/BookCase",
+		desc: "This MERN stack application uses the Google Books API for getting information on books by their ISBN-13s, and makes recommendations to users.",
+		tech: [
+			<i className="devicon-mongodb-plain-wordmark tech-icon"></i>,
+			<i className="devicon-express-original-wordmark tech-icon"></i>,
+			<i className="devicon-react-original-wordmark tech-icon"></i>,
+			<i className="devicon-nodejs-plain-wordmark tech-icon"></i>,
+		],
+	},
+	{
+		id: 2,
+		name: "TicTacToe",
+		live: "https://tictactoe-sa.herokuapp.com/",
+		image: "/assets/tictactoe.JPG",
+		repo: "https://github.com/SAbbas2018/tictactoe",
+		desc: "An OOP Tic Tac Toe game users can play against another user on the same window, a computer with 3 levels of difficulty, or agains a friend in real time through web sockets.",
+		tech: [
+			<i className="devicon-express-original-wordmark tech-icon"></i>,
+			<i className="devicon-react-original-wordmark tech-icon"></i>,
+			<i className="devicon-nodejs-plain-wordmark tech-icon"></i>,
+		],
+	},
+	//   {
+	//     id: 3,
+	//     name: "NewScript",
+	//     live: "https://newscript-sa.herokuapp.com/",
+	//     image: "/assets/newscript.JPG",
+	//     repo: "https://github.com/SAbbas2018/newscript-react",
+	//     desc:
+	//       "This React and Node.js application obtains weather information for a given city, and news headlines for common categories.",
+	//     tech: [
+	//       <i className="devicon-express-original-wordmark tech-icon"></i>,
+	//       <i className="devicon-react-original-wordmark tech-icon"></i>,
+	//       <i className="devicon-nodejs-plain-wordmark tech-icon"></i>,
+	//     ],
+	//   },
+	{
+		id: 3,
+		name: "Carden",
+		live: "http://www.mycarden.com/",
+		image: "/assets/carden.JPG",
+		repo: "",
+		desc: "Create and send interactive cards. Instead of sending your friend a text for their next birthday, send them a webpage. Choose from templates or make your own!",
+		tech: [
+			<i className="devicon-express-original-wordmark tech-icon"></i>,
+			<i className="devicon-react-original-wordmark tech-icon"></i>,
+			<i className="devicon-nodejs-plain-wordmark tech-icon"></i>,
+			<i className="devicon-mongodb-plain-wordmark tech-icon"></i>,
+		],
+	},
 ];
 
 export { projectData };
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,50 +1,56 @@
 import React from "react";
 
 export default function Skills() {
-  return (
-    <section className="skills-section wrapper" id="skills">
-      <article className="skills-container web">
-        <header className="container-title skills-header">Web Dev</header>
-        <main className="skills-grid">
-          <i className="devicon-react-original-wordmark  skill"></i>
-          <i className="devicon-nodejs-plain-wordmark  skill"></i>
-          <i className="devicon-express-original-wordmark skill"></i>
-          <i className="devicon-javascript-plain  skill"></i>
-          <i className="devicon-html5-plain-wordmark skill"></i>
-          <i className="devicon-css3-plain-wordmark skill"></i>
-        </main>
-      </article>
-      <br />
-      <article className="skills-container programming">
-        <header className="container-title skills-header">
-          Programming Languages
-        </header>
-        <main className="skills-grid">
-          <i className="devicon-java-plain-wordmark skill"></i>
-          <i className="devicon-python-plain-wordmark  skill"></i>
-          {/* <i className="devicon-go-plain skill"></i> */}
-        </main>
-      </article>
-      <br />
-      <article className="skills-container databases">
-        <header className="container-title skills-header">Databases</header>
-        <main className="skills-grid">
-          <i className="devicon-mongodb-plain-wordmark  skill"></i>
-          <i className="devicon-postgresql-plain-wordmark skill"></i>
-          <i className="devicon-mysql-plain-wordmark skill"></i>
-        </main>
-      </article>
-      <br />
-      <article className="skills-container tools">
-        <header className="container-title skills-header">Tools</header>
-        <main className="skills-grid">
-          <i className="devicon-heroku-line-wordmark skill"></i>
-          <i className="devicon-git-plain-wordmark skill"></i>
-          <i className="devicon-github-original-wordmark  skill"></i>
-          {/* <i className="devicon-bootstrap-plain-wordmark skill"></i> */}
-        </main>
-      </article>
-      <footer className="skills-spacer spacer"></footer>
-    </section>
-  );
+	return (
+		<section className="skills-section wrapper" id="skills">
+			<article className="skills-container web">
+				<header className="container-title skills-header">
+					Web Dev
+				</header>
+				<main className="skills-grid">
+					<i className="devicon-react-original-wordmark  skill"></i>
+					<i className="devicon-nodejs-plain-wordmark  skill"></i>
+					<i className="devicon-express-original-wordmark skill"></i>
+					<i className="devicon-javascript-plain  skill"></i>
+					<i className="devicon-html5-plain-wordmark skill"></i>
+					<i className="devicon-css3-plain-wordmark skill"></i>
+					<i className="devicon-php-plain  skill"></i>
+				</main>
+			</article>
+			<br />
+			<article className="skills-container programming">
+				<header className="container-title skills-header">
+					Programming Languages
+				</header>
+				<main className="skills-grid">
+					<i className="devicon-java-plain-wordmark skill"></i>
+					<i className="devicon-python-plain-wordmark  skill"></i>
+					<i class="devicon-typescript-plain skill"></i>
+					{/* <i className="devicon-go-plain skill"></i> */}
+				</main>
+			</article>
+			<br />
+			<article className="skills-container databases">
+				<header className="container-title skills-header">
+					Databases
+				</header>
+				<main className="skills-grid">
+					<i className="devicon-mongodb-plain-wordmark  skill"></i>
+					<i className="devicon-postgresql-plain-wordmark skill"></i>
+					<i className="devicon-mysql-plain-wordmark skill"></i>
+				</main>
+			</article>
+			<br />
+			<article className="skills-container tools">
+				<header className="container-title skills-header">Tools</header>
+				<main className="skills-grid">
+					<i className="devicon-heroku-line-wordmark skill"></i>
+					<i className="devicon-git-plain-wordmark skill"></i>
+					<i className="devicon-github-original-wordmark  skill"></i>
+					{/* <i className="devicon-bootstrap-plain-wordmark skill"></i> */}
+				</main>
+			</article>
+			<footer className="skills-spacer spacer"></footer>
+		</section>
+	);
 }
